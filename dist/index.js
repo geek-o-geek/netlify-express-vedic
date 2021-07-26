4409e27dcb1998047e102063c52cef1efabf7c4c
geekOgeek <arjun.sisodia8@gmail.com>
2021-07-26 13:52:44 +0530

    serevr renewed

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+});
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
